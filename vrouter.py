9964dd1d9dfe21011140b2f402825a6462c02af1
Lanndon Rose <roselan@arch08.cis.gvsu.edu>
2018-10-27 17:11:42 -0400

    chat between two host with TCP works

--- vrouter.py
+++ vrouter.py
@@ -2,7 +2,7 @@
 
 ##
 #
-#Jake Young
+#Jake Young, Lanndon Rose
 #updated virtual router
 #
 ##
@@ -14,19 +14,22 @@ import binascii
 import time
 
 #1
-def mactobinary(mac):
-    return binascii.unhexlify(mac.replace(':', ''))
+#def mactobinary(mac):
+#    return binascii.unhexlify(mac.replace(':', ''))
 #1
 #from http://stackoverflow.com/questions/2986702/need-some-help-converting-a-mac-address-to-binary-data-for-use-in-an-ethernet-fr
 
-routing = "global"
 class vrouter(object):
-    
+
+    def mactobinary(self, mac):
+        return binascii.unhexlify(mac.replace(':', ''))
+
     def readtable(self, name):
         rtable = []
         f=open(name, 'r')
-        for line in f:
-            rtable.append(line.replace("/"," ").rstrip().split(' '))
+        rtable = f.readlines()
+        #for line in f:
+        #    rtable.append(line.rstrip().split(' '))
         return rtable
 
     def __init__(self):
@@ -38,8 +41,8 @@ class vrouter(object):
 
         #read in routing table
         name = raw_input("Enter routing table name: ")
-        global routing
-        routing = self.readtable(name)
+        self.routing = self.readtable(name)
+        self.mactable = {}
         #print(self.routing)
 
     def sniff(self):
@@ -59,7 +62,10 @@ class vrouter(object):
                 #is it addressed to us?
                 netlist = netifaces.interfaces()
                 dest = socket.inet_ntoa(aHeader[8])
-                print aHeader[4]
+                if aHeader[4] == '\x00\x02':
+                    key = aHeader[6]
+                    if key in self.mactable:
+                        self.mactable[key] = aHeader[5]
 
                 for net in netlist:
                     myIP = netifaces.ifaddresses(net)[2][0]['addr']
@@ -67,11 +73,12 @@ class vrouter(object):
 
                     if dest == myIP:
                         if aHeader[4] == '\x00\x01':
-                            print 'Got an ARP request'                            
+                            key = aHeader[6]
+                            if key in self.mactable:
+                                self.mactable[key] = aHeader[5]
+                            print 'Got an ARP request'
                             dMAC = myMAC
                             self.respondToArp(packet, dMAC)
-                            elif aHeader[4] == '\x00\x02':
-                                
             elif eType == '\x08\x00':
                 ih = packet[0][14:34]
                 iheader = struct.unpack("1s1s2s2s2s1s1s2s4s4s", ih)
@@ -86,29 +93,144 @@ class vrouter(object):
                             myIP = netifaces.ifaddresses(net)[2][0]['addr']
                             myMAC = netifaces.ifaddresses(net)[17][0]['addr']
                             if dest == myIP:
-                                print 'got an icmp request'                                
+                                print 'got an icmp request'
                                 dMAC = myMAC
                                 self.respondToIcmp(packet, dMAC)
-                            else:
-                                print 'Not for me need to route'
-                                #trying to match the destination ip with and ip in the table to find
-                                #an interface to send another arp over to 
-                                #having trouble matching the destination ip with the ips
-                                #in the file
-                                #if dest == self.check(dest):
-                                    #print 'found one'
-                                    #the 3 spot in the table file is the interface, just trying to 
-                                    #matht the interface to send packet over
-                                if net == self.check(dest) :
-                                    print 'Not for me need to route'                                        
-                                    self.respondToArp(packet,dest)
+                else:
+                    #forward others
+                    dip = iheader[9]
+                    dest = socket.inet_ntoa(iheader[9])
+                    fDest = self.check(dest)
+                    if fDest == 'nomatch':
+                        #print 'no ip match found'
+                        continue
+                        #maybe exit or break
+                    key = dip
+                    print key
+                    if key in self.mactable:
+                        forwardMAC = self.mactable[dip]
+                        self.forward(packet,forwardMAC)
+                    else:
+                        forwardMAC = self.getMAC(packet, fDest, dip)
 
-                #elif protocol == '\x06':
-                #   print 'n'
-                    #tcp
-                #elif protocol == '\x11':
-                #   print 'n'
-                    #udp
+                    self.forward(packet, forwardMAC)
+                    #getMAC //sends arp and returns mac
+                    #forward //fixes ethernet header and sends
+                    
+    def forward(self, packet, mac):
+        e = packet[0][0:14]
+        eheader = list(struct.unpack("!6s6s2s", e))
+        temp = eheader
+        eheader[0] = mac
+        eheader[1] = temp[0]
+        fe = struct.pack("6s6s2s", *eheader)
+        finalPacket = fe+packet[0][14:]
+        #self.sock.sendto(finalPacket, packet[1]) #i dont think packet[1] is right
+        p = list(packet[1])
+        p[0] = self.interface.strip()
+        p[3] = int(self.interface[6:7])
+        addr = tuple(p)
+        self.sock.sendto(finalPacket, addr)
+        print 'packet forwarded'
+
+    def getMAC(self, packet, nexthop, dip):
+        e = packet[0][0:14]
+        newE = list(struct.unpack("!6s6s2s", e))
+        temp = e
+        newE[0] = self.mactobinary('ff:ff:ff:ff:ff:ff:')
+        print self.interface[3:]
+        newE[1] = self.mactobinary(netifaces.ifaddresses(self.interface.strip())[17][0]['addr'])#temp[0]
+        newE[2] = '\x08\x06'
+        finalE = struct.pack("6s6s2s", *newE)
+        
+        #sendE = []
+        #sendE.append(self.mactobinary('ff:ff:ff:ff:ff:ff'))
+        #sendE.append(newE[0])
+        #sendE.append('\x08\x06')
+        #finalE = struct.pack("6s6s2s", *sendE)
+        
+        i = packet[0][14:34]
+        newI = struct.unpack("1s1s2s2s2s1s1s2s4s4s", i)
+        
+        a = packet[0][14:42]
+        ahdr = struct.unpack("2s2s1s1s2s6s4s6s4s", a)
+        aheader = list(ahdr)
+        aheader[0] = '\x00\x01'
+        aheader[1] = '\x08\x00'
+        aheader[2] = '\x06'
+        aheader[3] = '\x04'
+        aheader[4] = '\x00\x01'
+        myip=''
+        netlist = netifaces.interfaces()
+        for net in netlist:
+            myIP = netifaces.ifaddresses(net)[2][0]['addr']
+            myMAC = netifaces.ifaddresses(net)[17][0]['addr']
+            if self.mactobinary(myMAC) == newE[0]:
+                #myip = myIP
+                myip = socket.inet_aton(myIP)
+        aheader[5] = self.mactobinary(netifaces.ifaddresses(self.interface.strip())[17][0]['addr'])
+        #if myip == '':
+        #    sys.exit(-1)
+        aheader[6] = socket.inet_aton(netifaces.ifaddresses(self.interface.strip())[2][0]['addr']) #myip
+        aheader[7] = self.mactobinary('00:00:00:00:00:00')
+        
+        if nexthop == '-' or nexthop == '':
+            nexthop = newI[9]
+        aheader[8] = nexthop
+        sendA = struct.pack("2s2s1s1s2s6s4s6s4s", *aheader)
+        sendpacket = finalE+sendA
+        #self.sock.sendto(sendpacket, (socket.inet_aton(myip), net)) #packet[1] is not a right destination
+        for x in packet[1]:
+            print x
+        p = list(packet[1])
+        p[0] = self.interface.strip()
+        p[3] = int(self.interface[6:7])
+        addr = tuple(p)
+        for y in addr:
+            print y
+        asock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x003))
+        asock.sendto(sendpacket, addr)
+        count = 0
+        while count == 0:
+            newerpacket = asock.recvfrom(1024)
+            q = newerpacket[0][14:42]
+            qq = struct.unpack("2s2s1s1s2s6s4s6s4s", q)
+            print count
+            if qq[4] == '\x00\x02':
+                count = count + 1
+        asock.close()
+        ############################
+        #self.sock.sendto(sendpacket, addr)  #try putting interface num for port              #packet[1])                #(net, 1024, 0, int(net[6:]), myip)) no error but doesn't show in wireshark
+        #print 'arp request sent'
+        
+        #time.sleep(1)
+        #newerpacket = self.sock.recv(1024)    
+        e2 = newerpacket[0][0:14]
+        e3 = struct.unpack("!6s6s2s", e)
+        print 'forward mac', e3[1]
+        self.mactable[dip] = e3[1]
+        return e3[1]
+        #############################
+
+    def check(self, dest):
+        #routing is the table
+        maps = {'16':4, '24':6}
+        for addr in self.routing:
+            a = addr.split(' ')
+            mask = a[0][9:]
+            if dest[0:maps[mask]] == a[0][0:maps[mask]]:
+                print 'found match'
+                self.interface = a[2]
+                return a[1]
+        return 'nomatch'
+                #if a[1] != '-':
+                #    return a[1]
+                #else:
+                #    return '-'
+                    #found matching ip and the next hop is not null
+                    #ping router and forward to router
+                    #else if next hop is null ping dest and forward
+
 
     def respondToIcmp(self, packet, dMAC):
         e = struct.unpack("!6s6s2s", packet[0][0:14])
@@ -135,14 +257,14 @@ class vrouter(object):
         e = struct.unpack("!6s6s2s", packet[0][0:14])
         eh = list(e)
         eh[0] = e[1]
-        eh[1] = mactobinary(dMAC)
+        eh[1] = self.mactobinary(dMAC)
         a = struct.unpack("2s2s1s1s2s6s4s6s4s", packet[0][14:42])
         ah = list(a)
         ah[4] = '\x00\x02'
         ah[6] = a[8]
         ah[8] = a[6]
         ah[7] = a[5]
-        ah[5] = mactobinary(dMAC)
+        ah[5] = self.mactobinary(dMAC)
         #pack n send
         newE = struct.pack("6s6s2s", *eh)
         newA = struct.pack("2s2s1s1s2s6s4s6s4s", *ah)
@@ -150,33 +272,6 @@ class vrouter(object):
         self.sock.sendto(sendPacket, packet[1])
         print 'arp response sent'
 
-    #new method to send and ARP to fine the mac for the next node instead of sending mac
-    #it send the ip of the mac it is looking for
-    def SendingArp(self, packet, dest):
-        e = struct.unpack("!6s6s2s", packet[0][0:14])
-        eh = list(e)
-        eh[0] = e[1]
-        eh[1] = mactobinary(netifaces.ifaddresses(net)[17][0]['addr'])
-        a = struct.unpack("2s2s1s1s2s6s4s6s4s", packet[0][14:42])
-        ah = list(a)
-        ah[4] = '\x00\x02'
-        ah[6] = a[8]
-        ah[8] = dest
-        ah[7] = a[5]
-        ah[5] = mactobinary(netifaces.ifaddresses(net)[17][0]['addr'])
-        #pack n send
-        newE = struct.pack("6s6s2s", *eh)
-        newA = struct.pack("2s2s1s1s2s6s4s6s4s", *ah)
-        sendPacket = newE+newA
-        self.sock.sendto(sendPacket, packet[1])
-        print 'arp response sent'
-
-    def check(self,dest):
-        maps = {'16':4,'24':6}
-        for addr in routing:            
-            if dest[:maps[addr[1]]] == addr[0][:maps[addr[1]]]:
-                print 'found match'
-                return addr[3]
 
 def main(argv):
     router = vrouter()
